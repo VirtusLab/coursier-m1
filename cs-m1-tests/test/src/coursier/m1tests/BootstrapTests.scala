a92acee3ff682e8b9bb40ca1d00e5c880d3ea613
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-10-13 15:18:22 +0200

    Updates

--- cs-m1-tests/test/src/coursier/m1tests/BootstrapTests.scala
+++ cs-m1-tests/test/src/coursier/m1tests/BootstrapTests.scala
@@ -2,6 +2,10 @@ package coursier.m1tests
 
 object BootstrapTests extends coursier.clitests.BootstrapTests {
   def launcher = Launcher.launcher
+  def assembly = Launcher.launcher
+
+  override def acceptsDOptions = false
+  override def acceptsJOptions = false
 
   override def enableNailgunTest = false
 }
