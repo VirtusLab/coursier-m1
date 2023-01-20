77cc1b5f7a6e337fcb75b02e6375bbc9565b9f3f
Łukasz Wroński <lukaszwronski98@gmail.com>
2023-01-20 14:07:18 +0100

    Add generating sdkman packages with cs

--- .github/scripts/build-linux-aarch64-from-docker.sh
+++ .github/scripts/build-linux-aarch64-from-docker.sh
@@ -2,7 +2,7 @@
 set -e
 
 apt-get update -q -y
-apt-get install -q -y build-essential libz-dev zlib1g-dev git python3-pip curl
+apt-get install -q -y build-essential libz-dev zlib1g-dev git python3-pip curl zip
 
 export PATH="$(pwd)/utils:$PATH"
 
@@ -13,3 +13,4 @@ git config --global --add safe.directory "$(pwd)"
 ./mill -i "cs-m1.writeNativeImageScript" generate.sh ""
 ./generate.sh
 ./mill -i "cs-m1.copyToArtifacts" ./artifacts
+.github/scripts/generate-os-packages.sh
